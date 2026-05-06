77ed4207cc176052cb90ed5cdb7a585f13419cd6
Iván Zancajo <zancajoivan@gmail.com>
2026-05-06 17:23:19 +0200

    test(backend): aislar suite unitaria de tests api/integration (SCRUM-60)

--- backend/jest.unit.config.cjs
+++ backend/jest.unit.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['\\.api\\.test\\.ts$', '\\.integration\\.test\\.ts$'],
   maxWorkers: 1,
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
